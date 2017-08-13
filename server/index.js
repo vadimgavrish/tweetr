3564594226109c29056db9de51934524d8d70e11
vadimgavrish <vadim@gavrish.ca>
2017-08-13 09:08:39 -0400

    removed extra files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 "use strict";
 
 // Basic express setup:
-const PORT          = 8080;
+const PORT          = 8080
+;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
